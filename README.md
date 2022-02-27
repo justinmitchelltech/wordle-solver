d1f052c19e94d93c2da691cb95d2fc63d214eb82
justinmitchelltech <74476402+justinmitchelltech@users.noreply.github.com>
2022-02-27 14:43:32 -0500

    Cleaning up data processing

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 *I'm going to cleanup the code and document how to use this tool by Feb 27 (end of the weekend).*
 
 ## Refrences/Credits
-1. [Long list of english words](https://github.com/dwyl/english-words/blob/master/words_alpha.txt)
+1. [List of all English words](https://github.com/dwyl/english-words/blob/master/words_alpha.txt)
+1. [20,000 most common English words](https://github.com/first20hours/google-10000-english/blob/master/20k.txt)
 1. [3,000 most common English words](https://www.ef.edu/english-resources/english-vocabulary/top-3000-words/)
 1. [Wordle Archive](https://www.devangthakkar.com/wordle_archive/?1) of past games (for testing)
--- script_prepare-data.py
+++ script_prepare-data.py
@@ -1,25 +1,37 @@
-from tools.loading import print_loading_bar
 import pandas as pd 
+from tools.loading import print_loading_bar
 
 
-fh = open("wordle-solver/data/words_3000_raw.txt")
+data_source_str = "wordle-solver/data/words_3000"  # Designates data to process
+
+# Open list of words
+fh = open(data_source_str + "_raw.txt")
 LENGTH = len(fh.readlines())  # get number of words 
-fh = open("wordle-solver/data/words_3000_raw.txt")  # reopen file handle 
+fh = open(data_source_str + "_raw.txt")  # reopen file handle 
 
+# Load letter frequency lookup table
+freqs = pd.read_csv("wordle-solver/data/lookup_char-freqs.csv", index_col=0)
+
+# Initialize data storage 
 words = pd.DataFrame()
 
-WORD_LENGTH = 5
+WORD_LENGTH = 5  # Word length setting (Wordle is currently a 5-letter game)
 
+# Process list of words 
 for i, word in enumerate(fh): 
 
-    print_loading_bar(i, LENGTH, title="Reading words: ", size=100, no_newline=True)
+    print_loading_bar(i, LENGTH, title="Preparing word data: ", size=100, no_newline=True)
 
     word = word.strip().lower()
 
     if len(word) == WORD_LENGTH:
         words.loc[i, 'word'] = word 
+        score = 0
         for j in range(WORD_LENGTH):
             col = 'char'+str(j)
             words.loc[i, col] = word[j]
+            score = freqs.loc[word[j], col] + score
+        words.loc[i, 'score'] = score
 
-words.to_csv("wordle-solver/data/words_3000_clean.csv")
+words = words.sort_values('score', ascending=False)
+words.to_csv(data_source_str + "_clean.csv")
